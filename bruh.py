680278dbc4ab7039fb08a2e95235a99d5c14003e
jolive1993 <rockitshipshawty@gmail.com>
2015-11-11 12:48:28 -0600

    semxy ui

--- bruh.py
+++ bruh.py
@@ -4,19 +4,22 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.image import AsyncImage
 
 
 class MainLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(MainLayout, self).__init__(**kwargs)
         self.orientation = "vertical"
-        self.buckets = 0
-        self.garbage = 0
+        self.spacing = 5
+        self.padding = 5
+        self.num1 = 0
+        self.num2 = 0
         self.funct = 0
-        self.tracker = TextInput(text=str(self.buckets))
+        self.tracker = Label(text=str(self.num1), font_size=50, halign='center')
         self.tracker.size_hint = (1, .1)
         self.add_widget(self.tracker)
-        buttons = GridLayout(cols=4)
+        buttons = GridLayout(cols=4, spacing=5)
         self.add_widget(buttons)
         butts = []
         for i in range(10):
@@ -42,59 +45,60 @@ class MainLayout(BoxLayout):
         }
 
     def divider(self,obj):
-        self.buckets = int(self.buckets)
-        self.garbage = self.buckets
-        self.buckets = 0
+        self.num1 = int(self.num1)
+        self.num2 = self.num1
+        self.num1 = 0
         self.funct = 4
     def multiplyer(self,obj):
-        self.buckets = int(self.buckets)
-        self.garbage = self.buckets
-        self.buckets = 0
+        self.num1 = int(self.num1)
+        self.num2 = self.num1
+        self.num1 = 0
         self.funct = 3
     def equals(self,obj):
 
         try:
-            buckets = int(self.buckets)
-            garbage = int(self.garbage)
+            num1 = int(self.num1)
+            num2 = int(self.num2)
             if self.funct == 1:
-                self.buckets = buckets +  garbage
+                self.num1 = num1 +  num2
             elif self.funct == 2:
-                self.buckets = garbage - buckets
+                self.num1 = num2 - num1
             elif self.funct == 3:
-                self.buckets = garbage * buckets
+                self.num1 = num2 * num1
             elif self.funct == 4:
-                self.buckets = garbage / buckets
-                self.buckets = float(self.buckets)
+                self.num1 = num2 / num1
+                self.num1 = float(self.num1)
         except (ValueError, ZeroDivisionError) as e:
             print(e)
-            self.buckets = 0
+            self.num1 = 0
 
-        
+  
     def adder(self, obj):
-        self.buckets = int(self.buckets)
-        self.garbage = self.buckets
-        self.buckets = 0
+        self.num1 = int(self.num1)
+        self.num2 = self.num1
+        self.num1 = 0
         self.funct = 1
     def minuser(self,obj):
-        self.buckets = int(self.buckets)
-        self.garbage = self.buckets
-        self.buckets = 0
+        self.num1 = int(self.num1)
+        self.num2 = self.num1
+        self.num1 = 0
         self.funct = 2
     def numclear(self, obj):
-        self.buckets = 0
-    def press(self,obj):
+        self.num1 = 0
+    def press(self, obj):
         try:
             int(obj.text)
-            self.buckets = str(self.buckets) + str(obj.text)
-            self.tracker.text = str(float(self.buckets))
+            self.num1 = str(self.num1) + str(obj.text)
+            self.tracker.text = str(float(self.num1))
         except ValueError:
             self.symbolFunctions[obj.text](obj)
-            self.tracker.text = str(float(self.buckets))
+            self.tracker.text = str(float(self.num1))
 
 
 class CalcApp(App):
     def build(self):
-        return MainLayout()
+        root = MainLayout()
+        return root
 
 if __name__ == "__main__":
     CalcApp().run()
